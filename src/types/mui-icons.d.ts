b723c00bf6b5eba76ad21a2c5664dbdc66e77549
hitarthganatra-personal <hitarthganatra@gmail.com>
2026-05-29 12:16:08 +0530

    Ready for initial deployment

---
+++ src/types/mui-icons.d.ts
@@ -0,0 +1,5 @@
+declare module '@mui/icons-material/TravelExplore' {
+  import * as React from 'react';
+  const TravelExploreIcon: React.FC<React.SVGProps<SVGSVGElement>>;
+  export default TravelExploreIcon;
+}
